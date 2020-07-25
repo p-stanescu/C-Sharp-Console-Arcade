e4b5dc1edb5ed0199acbb6428f3f3f056f3857f9
Petru <petru@Petrus-MacBook.local>
2020-07-25 19:30:55 +0100

    written the test for the RandomNumberGenerator function

--- Program.cs
+++ Program.cs
@@ -8,7 +8,15 @@ namespace c_sharp_console_arcade
         {
             Console.WriteLine("Welcome to the C# version of rock, paper, scissors! This your chance to play this classic game against a computer. To start off, please enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine($"Hello {name}! Now, choose rock, paper or scissors: ");
+            string userChoice = Console.ReadLine();
+            string lowerUserChoice = userChoice.ToLower();
+            Console.WriteLine(RandomNumberGenerator());
+        }
+
+        public static int RandomNumberGenerator()
+        {
+            
         }
 
     }
--- testclass.cs
+++ testclass.cs
@@ -3,5 +3,9 @@ using c_sharp_console_arcade;
 
 public class testclass
 {
-  
+  [Fact]
+  public void RandomNumberGeneratorTest()
+  {
+    Assert.InRange(Program.RandomNumberGenerator(), 1, 4);
+  }
 }
