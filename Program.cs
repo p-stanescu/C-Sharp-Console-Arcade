d6078316927627904c0aa293d3741452b68242a8
Petru <petru@Petrus-MacBook.local>
2020-07-27 17:42:15 +0100

    changed to Console.Write in the main function to type in line

--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace c_sharp_console_arcade
         
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the C# version of rock, paper, scissors! This your chance to play this classic game against a computer. To start off, please enter your name: ");
+            Console.Write("Welcome to the C# version of rock, paper, scissors! This your chance to play this classic game against a computer. To start off, please enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine($"Hello {name}! Now, choose rock, paper or scissors: ");
+            Console.Write($"Hello {name}! Now, choose rock, paper or scissors: ");
             string userChoice = Console.ReadLine();
             string lowerUserChoice = userChoice.ToLower();
             int randomNumber = RandomNumberGenerator();
