5b66b672c5b0301f0172e4d7486999263f28b2eb
Petru <petru@Petrus-MacBook.local>
2020-07-27 17:40:25 +0100

    finished adding comments to the testclass.cs

--- testclass.cs
+++ testclass.cs
@@ -4,11 +4,15 @@ using c_sharp_console_arcade;
 public class testclass
 {
 
+  // Test function to check the numbers generated are only between 0 and 4
+
   [Fact]
   public void RandomNumberGeneratorTest()
   {
     Assert.InRange(Program.RandomNumberGenerator(), 1, 4);
   }
+
+  // Test function the outcome of the ComputerChoice function based on all the numbers
 
   [Fact]
   public void ComputerChoiceTest()
@@ -17,6 +21,8 @@ public class testclass
     Assert.Equal("paper", Program.ComputerChoice(2));
     Assert.Equal("scissors", Program.ComputerChoice(3));
   }
+
+  // Test function to check the outcome of the game based on all different choices
 
   [Fact]
   public void GameOutcomeTest()
